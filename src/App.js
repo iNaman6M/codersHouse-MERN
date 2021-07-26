eb478cb9aad114bdf23acdd827f98ef2f592cc7e
Emir-Tarun16 <naman.bkarogyam@gmail.com>
2021-07-27 00:53:24 +0530

    Guest page Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Navigation from './components/shared/Navigation/Navigation';
 import Register from './pages/Register/Register';
 import Login from './pages/Login/Login';
+import Authenticate from './pages/Authentication/Authenticate';
+
+const isAuth = true;
 
 function App() {
   return (
@@ -13,15 +16,52 @@ function App() {
           <Route path='/' exact>
             <Home />
           </Route>
-          <Route path='/register'>
-            <Register />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
+          <GuestRoute to='/authenticate'>
+            <Authenticate />
+          </GuestRoute>
+          
       </Switch>
    </BrowserRouter>
   );
 }
 
+//creating protected routes
+const GuestRoute = ({children,...rest}) => {
+  return ( 
+    <Route {...rest}
+      render={({location}) => {
+        return isAuth ? 
+        <Redirect to={
+            {
+              pathname: '/rooms',
+              state: {from: location}
+            }
+        }
+        />
+        :
+        (
+          children
+        );
+      }}        
+    > 
+
+  </Route>
+
+  );
+}
+
+
 export default App;
+
+
+
+
+//  <Route path='/register'>
+//             <Register />
+//           </Route>
+//           <Route path="/login">
+//             <Login />
+//           </Route> 
+//            <Route path='/authenticate'>
+//             <Authenticate />
+//           </Route> 
